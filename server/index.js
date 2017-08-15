4102f56d4991548a0cb6e9686214a9fd61a6256b
Sidney Packer <sidp@knights.ucf.edu>
2017-08-15 17:28:47 -0400

    Use server side compression middleware

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const compression = require('compression');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('port', PORT);
+app.use(compression());
 app.use(express.static(path.resolve(__dirname, '../build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../build/index.html'));
